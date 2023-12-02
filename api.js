63b4b4931d3cdfe2489e559af6a90cde35677cc2
kschw964 <kschw964@mtroyal.ca>
2023-12-01 23:18:28 -0700

    Implement paintings API

--- api.js
+++ api.js
@@ -4,6 +4,7 @@ const express = require("express");
 
 const artists = getJSONData("artists.json");
 const galleries = getJSONData("galleries.json");
+const paintings = getJSONData("paintings-nested.json");
 
 const app = express();
 
@@ -29,6 +30,58 @@ app.get("/api/galleries/:country", (req, resp) => {
       gallery.GalleryCountry.toLowerCase() == req.params.country.toLowerCase()
   );
   resp.json(foundArtists);
+});
+
+app.get("/api/paintings", (req, resp) => {
+  resp.json(paintings);
+});
+
+const paintingPath = "/api/painting";
+
+app.get(paintingPath + "/:id", (req, resp) => {
+  const foundPainting = paintings.filter(
+    (painting) => painting.paintingID == req.params.id
+  );
+  resp.json(foundPainting);
+});
+
+app.get(paintingPath + "/gallery/:id", (req, resp) => {
+  const foundPainting = paintings.filter(
+    (painting) => painting.gallery.galleryID == req.params.id
+  );
+  resp.json(foundPainting);
+});
+
+app.get(paintingPath + "/artist/:id", (req, resp) => {
+  const foundPainting = paintings.filter(
+    (painting) => painting.artist.artistID == req.params.id
+  );
+  resp.json(foundPainting);
+});
+
+app.get(paintingPath + "year/:min/:max", (req, resp) => {
+  const foundPainting = paintings.filter(
+    (painting) =>
+      painting.yearOfWork >= req.params.min &&
+      painting.yearOfWork <= req.params.min
+  );
+  resp.json(foundPainting);
+});
+
+app.get(paintingPath + "/title/:text", (req, resp) => {
+  const foundPainting = paintings.filter((painting) =>
+    painting.title.contains(req.params.text)
+  );
+  resp.json(foundPainting);
+});
+
+app.get(paintingPath + "/color/:name", (req, resp) => {
+  const foundPainting = paintings.filter((painting) =>
+    painting.details.annotation.dominantColors.find(
+      (color) => color.name.toLowerCase() == req.params.name.toLowerCase()
+    )
+  );
+  resp.json(foundPainting);
 });
 
 const port = 8080;
