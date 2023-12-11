f20789b30179b002d79609478441e263907e0857
kschw964 <kschw964@mtroyal.ca>
2023-12-10 21:20:44 -0700

    Extract paintingsAPI into own module

--- api.js
+++ api.js
@@ -4,6 +4,7 @@ const { artists, galleries, paintings } = require("./dataProvider.js");
 
 const artistsAPI = require("./artistsAPI.js");
 const galleriesAPI = require("./galleriesAPI.js");
+const paintingsAPI = require("./paintingsAPI.js");
 
 const app = express();
 
@@ -13,97 +14,15 @@ artistsAPI.handleArtistsFromCountry(app, artists);
 galleriesAPI.handleAll(app, galleries);
 galleriesAPI.handleGalleriesInCountry(app, galleries);
 
-app.get("/api/paintings", (req, resp) => {
-  resp.json(paintings);
-});
-
-const paintingPath = "/api/painting";
-
-app.get(paintingPath + "/:id", (req, resp) => {
-  const foundPainting = paintings.find(
-    (painting) => painting.paintingID == req.params.id
-  );
-  if (foundPainting) resp.json(foundPainting);
-  else
-    sendNotFoundResponse(
-      resp,
-      "Not painting with the id " + req.params.id + " found"
-    );
-});
-
-app.get(paintingPath + "/gallery/:id", (req, resp) => {
-  const foundPaintings = paintings.filter(
-    (painting) => painting.gallery.galleryID == req.params.id
-  );
-  if (foundPaintings) resp.json(foundPaintings);
-  else
-    sendNotFoundResponse(
-      resp,
-      "No paintings from gallery with id " + req.params.id + " found"
-    );
-});
-
-app.get(paintingPath + "/artist/:id", (req, resp) => {
-  const foundPaintings = paintings.filter(
-    (painting) => painting.artist.artistID == req.params.id
-  );
-  if (foundPaintings) resp.json(foundPaintings);
-  else
-    sendNotFoundResponse(
-      resp,
-      "No paintings from the artist with id " + req.params.id + " found"
-    );
-});
-
-app.get(paintingPath + "year/:min/:max", (req, resp) => {
-  const foundPaintings = paintings.filter(
-    (painting) =>
-      painting.yearOfWork >= req.params.min &&
-      painting.yearOfWork <= req.params.min
-  );
-  if (foundPaintings) resp.json(foundPaintings);
-  else
-    sendNotFoundResponse(
-      resp,
-      "No paintings drawn between " +
-        req.params.min +
-        " and " +
-        req.params.max +
-        " found"
-    );
-});
-
-app.get(paintingPath + "/title/:text", (req, resp) => {
-  const foundPaintings = paintings.filter((painting) =>
-    painting.title.contains(req.params.text)
-  );
-  if (foundPaintings) resp.json(foundPaintings);
-  else
-    sendNotFoundResponse(
-      resp,
-      'No paintings with the title containing "' + req.params.text + '" found'
-    );
-});
-
-app.get(paintingPath + "/color/:name", (req, resp) => {
-  const foundPaintings = paintings.filter((painting) =>
-    painting.details.annotation.dominantColors.find(
-      (color) => color.name.toLowerCase() == req.params.name.toLowerCase()
-    )
-  );
-  if (foundPaintings) resp.json(foundPaintings);
-  else
-    sendNotFoundResponse(
-      resp,
-      "No paintings containing the color " + req.params.name + " found"
-    );
-});
+paintingsAPI.handleAll(app, paintings);
+paintingsAPI.handlePaintingWithID(app, paintings);
+paintingsAPI.handlePaintingsInGalleryWithID(app, paintings);
+paintingsAPI.handlePaintingsFromArtistWithID(app, paintings);
+paintingsAPI.handlePaintingsWithinTimeperiod(app, paintings);
+paintingsAPI.handlePaintingsWithTitle(app, paintings);
+paintingsAPI.handlePaintingsContainingColor(app, paintings);
 
 const port = 8080;
 app.listen(port, () => {
   console.log("Running on port: " + port);
 });
-
-function sendNotFoundResponse(resp, message) {
-  resp.json({ error: "Requested item(s) not found", details: message });
-}
---
+++ paintingsAPI.js
@@ -0,0 +1,108 @@
+const handleError = require("./errorHandler.js");
+
+function handleAll(app, paintings) {
+  app.get("/api/paintings", (req, resp) => {
+    resp.json(paintings);
+  });
+}
+
+const paintingPath = "/api/painting";
+
+function handlePaintingWithID(app, paintings) {
+  app.get(paintingPath + "/:id", (req, resp) => {
+    const foundPainting = paintings.find(
+      (painting) => painting.paintingID == req.params.id
+    );
+    if (foundPainting) resp.json(foundPainting);
+    else
+      handleError(resp, "Not painting with the id " + req.params.id + " found");
+  });
+}
+
+function handlePaintingsInGalleryWithID(app, paintings) {
+  app.get(paintingPath + "/gallery/:id", (req, resp) => {
+    const foundPaintings = paintings.filter(
+      (painting) => painting.gallery.galleryID == req.params.id
+    );
+    if (foundPaintings.length) resp.json(foundPaintings);
+    else
+      handleError(
+        resp,
+        "No paintings from gallery with id " + req.params.id + " found"
+      );
+  });
+}
+
+function handlePaintingsFromArtistWithID(app, paintings) {
+  app.get(paintingPath + "/artist/:id", (req, resp) => {
+    const foundPaintings = paintings.filter(
+      (painting) => painting.artist.artistID == req.params.id
+    );
+    if (foundPaintings.length) resp.json(foundPaintings);
+    else
+      handleError(
+        resp,
+        "No paintings from the artist with id " + req.params.id + " found"
+      );
+  });
+}
+
+function handlePaintingsWithinTimeperiod(app, paintings) {
+  app.get(paintingPath + "year/:min/:max", (req, resp) => {
+    const foundPaintings = paintings.filter(
+      (painting) =>
+        painting.yearOfWork >= req.params.min &&
+        painting.yearOfWork <= req.params.min
+    );
+    if (foundPaintings.length) resp.json(foundPaintings);
+    else
+      handleError(
+        resp,
+        "No paintings drawn between " +
+          req.params.min +
+          " and " +
+          req.params.max +
+          " found"
+      );
+  });
+}
+
+function handlePaintingsWithTitle(app, paintings) {
+  app.get(paintingPath + "/title/:text", (req, resp) => {
+    const foundPaintings = paintings.filter((painting) =>
+      painting.title.contains(req.params.text)
+    );
+    if (foundPaintings.length) resp.json(foundPaintings);
+    else
+      handleError(
+        resp,
+        'No paintings with the title containing "' + req.params.text + '" found'
+      );
+  });
+}
+
+function handlePaintingsContainingColor(app, paintings) {
+  app.get(paintingPath + "/color/:name", (req, resp) => {
+    const foundPaintings = paintings.filter((painting) =>
+      painting.details.annotation.dominantColors.find(
+        (color) => color.name.toLowerCase() == req.params.name.toLowerCase()
+      )
+    );
+    if (foundPaintings.length) resp.json(foundPaintings);
+    else
+      handleError(
+        resp,
+        "No paintings containing the color " + req.params.name + " found"
+      );
+  });
+}
+
+module.exports = {
+  handleAll,
+  handlePaintingWithID,
+  handlePaintingsInGalleryWithID,
+  handlePaintingsFromArtistWithID,
+  handlePaintingsWithinTimeperiod,
+  handlePaintingsWithTitle,
+  handlePaintingsContainingColor,
+};
